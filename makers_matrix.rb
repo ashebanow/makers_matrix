72bc30d42c607f6a69da7b55c5de3d2f952e5b0b
Andrew Shebanow <ashebanow@gmail.com>
2019-05-27 12:28:45 -0700

    Rejiggered the logic for handling the main loop, since the printed books have a different loop logic.

--- makers_matrix.rb
+++ makers_matrix.rb
@@ -8,170 +8,180 @@ class MakersMatrix
   attr_accessor :total_crystal_orbs
   attr_accessor :total_magecoins
 
+  def run_matrix(item_level, use_sooth_deck: false)
+    @components_used = []
+    @effects = []
+    @effects_lookup = MakerEffects.new;
+    @current_level = 1
+
+    @bene_added = ask_numeric_question("How many Intellect bene do you wish to use PER ROLL? (0-10, defaults to 0) ", 0, 10)
+    @skill_bene = ask_numeric_question("How many relevant skill levels do you have? (0-10, defaults to 0) ", 0, 10)
+
+    # note that the material used is always at the desired level for the item
+    add_material(item_level)
+
+    while @current_level <= item_level
+      if roll_challenge(@current_level)
+        add_ingredient
+      else
+        puts "FAILURE attempting to add L%d ingredient" % [ @current_level ]
+        add_catalyst
+
+        if roll_challenge(@current_level)
+          puts "Successfully used L%d catalyst" % [ @current_level - 1 ]
+          @effects.push(SideEffect.new(:minor_effect, @current_level))
+          # add a higher level ingredient than originally planned
+          add_ingredient
+        else
+          puts "FAILURE using L%d catalyst" % [ @current_level - 1 ]
+          add_stabilizer
+
+          if roll_challenge(@current_level)
+            puts "Successfully used L%d stabilizer" % [ @current_level - 1 ]
+            @effects.push(SideEffect.new(:major_effect, @current_level))
+            # add a higher level ingredient than originally planned
+            add_ingredient
+          else
+            puts "FAILURE using L%d stabilizer" % [ @current_level - 1 ]
+            puts "Item Creation FAILED"
+            @effects.push(SideEffect.new(:mishap, @current_level))
+            print_results
+            return
+          end
+        end
+
+        # ask user whether or not they wish to continue.
+        if !ask_yes_no_question("Do you wish to continue? (Y/n) ")
+          # if not, create random item at current level'
+          puts "Created a RANDOM L%d item" % [@current_level]
+          print_results
+          return
+        end
+      end
+
+    end
+
+    add_power_source
+    # Note that last challenge is at higher level than desired item level
+    if roll_challenge(@current_level + 1)
+      puts "Successfully created item"
+    else
+      puts "FAILURE creating item"
+      @effects.push(SideEffect.new(:mishap, @current_level))
+    end
+
+    print_results
+  end
+
+private
+  def add_material(level)
+    push_component(:material, level)
+    puts "Added L%d material" % [ level ]
+  end
+
+  def add_ingredient
+    push_component(:ingredient, @current_level)
+    puts "Added L%d ingredient" % [ @current_level ]
+    @current_level += 1
+  end
+
+  def add_catalyst
+    push_component(:catalyst, @current_level)
+    puts "Added L%d catalyst" % [ @current_level ]
+    @current_level += 1
+  end
+
+  def add_stabilizer
+    push_component(:stabilizer, @current_level)    
+    puts "Added L%d stabilizer" % [ @current_level ]
+    @current_level += 1
+  end
+
+  def add_power_source
+    push_component(:power_source, @current_level)
+    puts "Added L%d power source" % [ @current_level ]
+  end
+
   # all values are in Crystal Orbs (CO). One orb is 0.01 CO, one magecoin is 200 CO but
   # is specified as a separate entry per row
-  MaterialCost = Struct.new(:value, :magecoins) do
+  ComponentCost = Struct.new(:value, :magecoins) do
     def to_ary
       [value, magecoins]
     end
   end
 
-  MaterialUsed = Struct.new(:material_type, :level, :value, :magecoins) do
+  ComponentUsed = Struct.new(:type, :level, :value, :magecoins) do
   end
 
   SideEffect = Struct.new(:type, :level) do
   end
 
-  def run_matrix(item_level, use_sooth_deck: false)
-    @materials = []
-    @effects = []
-    @effects_lookup = MakerEffects.new;
-    level = 1
-
-    @bene_added = ask_numeric_question("How many Intellect bene do you wish to use PER ROLL? (0-10, defaults to 0) ", 0, 10)
-    @skill_bene = ask_numeric_question("How many relevant skill levels do you have? (0-10, defaults to 0) ", 0, 10)
-
-    add_material(item_level)
-    while level <= item_level
-      failed = add_ingredient(level)
-      level += 1
-      # ask user whether or not they wish to continue.
-      if failed && !ask_yes_no_question("Do you wish to continue? (Y/n) ")
-        # if not, create random item at current level'
-        puts "Created a RANDOM L%d item" % [level]
-        print_results
-        return
-      end
-    end
-    add_power_source(level)
-
-    print_results
-  end
-
-  def add_material(level)
-    push_material(:material, level)
-    puts "Added L%d material successfully" % [ level ]
-  end
-
-  def add_ingredient(level)
-    failed = false
-
-    push_material(:ingredient, level)
-
-    if roll_challenge(:ingredient, level)
-      puts "Added L%d ingredient successfully" % [ level ]
-    else
-      puts "FAILURE adding L%d ingredient" % [ level ]
-      # if challenge fails, add_catalyst at level + 1
-      add_catalyst(level + 1)
-      failed = true
-    end
-
-    return failed
-  end
-
-  def add_catalyst(level)
-    push_material(:catalyst, level)    
-
-    if roll_challenge(:catalyst, level)
-      puts "Added L%d catalyst successfully" % [ level ]
-      @effects.push(SideEffect.new(:minor_effect, level))
-    else
-      puts "FAILURE adding L%d catalyst" % [ level ]
-      add_stabilizer(level + 1)
-    end
-  end
-
-  def add_stabilizer(level)
-    push_material(:stabilizer, level)    
-
-    if roll_challenge(:stabilizer, level)
-      puts "Added L%d stabilizer successfully" % [ level ]
-      @effects.push(SideEffect.new(:major_effect, level))
-    else
-      puts "FAILURE adding L%d stabilizer" % [ level ]
-      @effects.push(SideEffect.new(:mishap, level))
-    end
-  end
-
-  def add_power_source(level)
-    push_material(:power_source, level)    
-
-    if roll_challenge(:power_source, level)
-      puts "Added L%d power source successfully" % [ level ]
-    else
-      puts "FAILURE adding L%d power source" % [ level ]
-      @effects.push(SideEffect.new(:mishap, level))
-    end
-  end
-
-private
   MATERIALS = [
-    MaterialCost.new( 10,  0),
-    MaterialCost.new( 25,  0),
-    MaterialCost.new( 75,  0),
-    MaterialCost.new(200,  0),
-    MaterialCost.new(500,  0),
-    MaterialCost.new(  0,  1),
-    MaterialCost.new(  0,  3),
-    MaterialCost.new(  0,  7),
-    MaterialCost.new(  0, 20),
-    MaterialCost.new(  0, 50)
+    ComponentCost.new( 10,  0),
+    ComponentCost.new( 25,  0),
+    ComponentCost.new( 75,  0),
+    ComponentCost.new(200,  0),
+    ComponentCost.new(500,  0),
+    ComponentCost.new(  0,  1),
+    ComponentCost.new(  0,  3),
+    ComponentCost.new(  0,  7),
+    ComponentCost.new(  0, 20),
+    ComponentCost.new(  0, 50)
   ]
 
   INGREDIENTS = [
-    MaterialCost.new(0.1,   0),
-    MaterialCost.new(0.5,   0),
-    MaterialCost.new(  2,   0),
-    MaterialCost.new( 10,   0),
-    MaterialCost.new( 50,   0),
-    MaterialCost.new(200,   0),
-    MaterialCost.new(  0,   1),
-    MaterialCost.new(  0,   5),
-    MaterialCost.new(  0,  12),
-    MaterialCost.new(  0,  30)
+    ComponentCost.new(0.1,   0),
+    ComponentCost.new(0.5,   0),
+    ComponentCost.new(  2,   0),
+    ComponentCost.new( 10,   0),
+    ComponentCost.new( 50,   0),
+    ComponentCost.new(200,   0),
+    ComponentCost.new(  0,   1),
+    ComponentCost.new(  0,   5),
+    ComponentCost.new(  0,  12),
+    ComponentCost.new(  0,  30)
   ]
 
   CATALYSTS = [
-    MaterialCost.new(0.1,   0),
-    MaterialCost.new(0.5,   0),
-    MaterialCost.new(  2,   0),
-    MaterialCost.new( 10,   0),
-    MaterialCost.new( 50,   0),
-    MaterialCost.new(200,   0),
-    MaterialCost.new(  0,   1),
-    MaterialCost.new(  0,   5),
-    MaterialCost.new(  0,  12),
-    MaterialCost.new(  0,  30)
+    ComponentCost.new(0.1,   0),
+    ComponentCost.new(0.5,   0),
+    ComponentCost.new(  2,   0),
+    ComponentCost.new( 10,   0),
+    ComponentCost.new( 50,   0),
+    ComponentCost.new(200,   0),
+    ComponentCost.new(  0,   1),
+    ComponentCost.new(  0,   5),
+    ComponentCost.new(  0,  12),
+    ComponentCost.new(  0,  30)
   ]
 
   STABILIZERS = [
-    MaterialCost.new(0.1,   0),
-    MaterialCost.new(0.5,   0),
-    MaterialCost.new(  2,   0),
-    MaterialCost.new( 10,   0),
-    MaterialCost.new( 50,   0),
-    MaterialCost.new(200,   0),
-    MaterialCost.new(  0,   1),
-    MaterialCost.new(  0,   5),
-    MaterialCost.new(  0,  12),
-    MaterialCost.new(  0,  30)
+    ComponentCost.new(0.1,   0),
+    ComponentCost.new(0.5,   0),
+    ComponentCost.new(  2,   0),
+    ComponentCost.new( 10,   0),
+    ComponentCost.new( 50,   0),
+    ComponentCost.new(200,   0),
+    ComponentCost.new(  0,   1),
+    ComponentCost.new(  0,   5),
+    ComponentCost.new(  0,  12),
+    ComponentCost.new(  0,  30)
   ]
 
   POWER_SOURCES = [
-    MaterialCost.new( 25,  0),
-    MaterialCost.new( 50,  0),
-    MaterialCost.new( 75,  0),
-    MaterialCost.new(100,  0),
-    MaterialCost.new(125,  0),
-    MaterialCost.new(  0,  3),
-    MaterialCost.new(  0,  4),
-    MaterialCost.new(  0,  5),
-    MaterialCost.new(  0,  6),
-    MaterialCost.new(  0,  7)
+    ComponentCost.new( 25,  0),
+    ComponentCost.new( 50,  0),
+    ComponentCost.new( 75,  0),
+    ComponentCost.new(100,  0),
+    ComponentCost.new(125,  0),
+    ComponentCost.new(  0,  3),
+    ComponentCost.new(  0,  4),
+    ComponentCost.new(  0,  5),
+    ComponentCost.new(  0,  6),
+    ComponentCost.new(  0,  7)
   ]
 
-  MATERIAL_TYPE_MAP = {
+  COMPONENT_TYPE_MAP = {
     :material => MATERIALS,
     :ingredient => INGREDIENTS,
     :catalyst => CATALYSTS,
@@ -196,28 +206,27 @@ private
       ]
     }
 
-    @total_crystal_orbs = @materials.inject(0.0) { |crystal_orbs, material|
+    @total_crystal_orbs = @components_used.inject(0.0) { |crystal_orbs, material|
       crystal_orbs += material.value
     }
-    @total_magecoins = @materials.inject(0.0) { |magecoins, material|
+    @total_magecoins = @components_used.inject(0.0) { |magecoins, material|
       magecoins += material.magecoins
     }
     puts "Total cost to make item: %.2f Crystal Orbs, %.2f Magecoins" % [@total_crystal_orbs, @total_magecoins]
   end
 
-  def push_material(type, level)
-    value, magecoins = MATERIAL_TYPE_MAP[type][level - 1]
-    material_used = MaterialUsed.new(type, level, value, magecoins)
-    @materials.push(material_used)
-    return material_used
+  def push_component(type, level)
+    value, magecoins = COMPONENT_TYPE_MAP[type][level - 1]
+    component_used = ComponentUsed.new(type, level, value, magecoins)
+    @components_used.push(component_used)
+    return component_used
   end
 
-  def roll_challenge(type, level)
+  def roll_challenge(level)
     dice = Dice.new(1, 10)
     total_bene = @bene_added + @skill_bene
     dice += total_bene if total_bene > 0
-    roll = dice.best(1)
-    return roll >= level
+    return dice.roll >= level
   end
 
   def ask_yes_no_question(prompt, default_true=true)
